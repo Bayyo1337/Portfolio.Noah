24c3bd76770a31d198b4885dfa256353c43600a2
Noah Richter <noah.richter14@gmail.com>
2026-01-29 11:03:09 +0100

    fix: implemented new TerminalCommand (hopefully) correctly

--- Portfolio.Marvin/Extensions/ServiceCollectionExtensions.cs
+++ Portfolio.Marvin/Extensions/ServiceCollectionExtensions.cs
@@ -34,6 +34,7 @@ public static class ServiceCollectionExtensions
          .AddSingleton<ITerminalCommand, SkillsCommand>()
          .AddSingleton<ITerminalCommand, NiklasCommand>()
          .AddSingleton<ITerminalCommand, LukasCommand>()
+         .AddSingleton<ITerminalCommand, NoahCommand>()
          .AddSingleton<ITerminalCommand, SudoCommand>()
          .AddSingleton<ITerminalCommand, HireCommand>();
    }
